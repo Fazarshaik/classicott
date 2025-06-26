3b38e199208618b9205c81d0858623f4995cb1f2
meheriswar <meheriswar7447@gmail.com>
2025-06-27 00:45:10 +0530

    meher

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import ForgotPassword from './pages/Forgotpassword';
 import Categories from './pages/Categories';
-import MoviePage from './pages/MoviesPage'; 
+import MoviesPage from './pages/MoviesPage';
 import Home from './components/Home';
 import VintageSubscriptionPage from './pages/Subscription';
 import PaymentPage from './pages/Payment';
@@ -13,16 +13,19 @@ import ClassicFooter from './pages/Footer';
 
 const App = () => {
   return (
-    <>
     <Routes>
-      <Route path='/' element={<Signup />}/>
+      <Route path='/' element={<Signup />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/categories' element={<Categories />} />
+      <Route path="/movies" element={<MoviesPage />} />
+
       <Route path='/home' element={<Home />} />
       <Route path='/subscription' element={<VintageSubscriptionPage />} />
-      <Route path='/payment' element={<PaymentPage/>} />
-      <Route path='/video' element={<CricketVideoPage/>} />
-      <Route path='/footer' element={<ClassicFooter/>} />
+      <Route path='/payment' element={<PaymentPage />} />
+      <Route path='/video' element={<CricketVideoPage />} />
+      <Route path='/footer' element={<ClassicFooter />} />
     </Routes>
-    </>
   );
 };
 
